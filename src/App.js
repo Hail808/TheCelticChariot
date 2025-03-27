a473ac305c94f28ec70bce5771e0d4fc90407509
software45 <167963162+software45@users.noreply.github.com>
2025-03-27 00:18:20 -0700

    ccw 91
    Create the cart.tsx page with the header, which will have the full list of customer items in cart.

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Navbar from './components/navbar.tsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/home.tsx'
+import Login from './components/pages/login.tsx'
+import ForgotPassword from './components/pages/forgot_password.tsx';
+import Footer from './components/footer.tsx';
+import About from './components/pages/about_me.tsx';
+import Cart from './components/pages/cart.tsx'; 
+import Review from './components/pages/reviews.tsx';
+
+function App() {
+  return (
+    <div className="App"> 
+      <Router>
+        <Navbar />
+        {/* Content wrapper to expand and push footer down */}
+        <main className="content">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about_me' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path="/forgot_password" element={<ForgotPassword />} />
+            <Route path="/reviews" element={<Review />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
