db213f2b99b633fbc3fd220cfbbf55f3aa2fb082
OddAmbition <jqmariano@csus.edu>
2025-03-26 20:04:53 -0700

    added catalogue page and search function

---
+++ src/app/catalogue/page.tsx
@@ -0,0 +1,7 @@
+import Catalogue from '../../components/pages/catalogue';  // Adjust the import path if needed
+
+const CataloguePage: React.FC = () => {
+  return <Catalogue />;
+};
+
+export default CataloguePage;
