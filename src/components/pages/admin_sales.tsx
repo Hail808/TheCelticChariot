3c28cf194a22140461cb7cbbbd8cf6baf643e88b
Hailoe <99317873+SuccededHail97@users.noreply.github.com>
2025-03-30 21:15:54 -0700

    CCW-127 - Add admin sales page and styles

--- src/components/pages/admin_sales.tsx
+++ src/components/pages/admin_sales.tsx
@@ -1,13 +1,65 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 
-import '../../styles/admin_home.css';
+import '../../styles/admin_sales.css';
+
+/* test table data*/
+const data = [
+    {
+        id: 1,
+        month: "January",
+        year: "2022",
+        totalViews: "15",
+        sales: "$100.00",
+        revenue: "$50.00"
+    },
+    {
+        id: 2,
+        month: "February",
+        year: "2022",
+        totalViews: "30",
+        sales: "$300.00",
+        revenue: "$250.00"
+    },
+    {
+        id: 3,
+        month: "March",
+        year: "2022",
+        totalViews: "10",
+        sales: "$80.00",
+        revenue: "$70.00"
+    },
+  ];
 
 const AdminSales: React.FC = () => {
     return (
         <div className="container">
             <h1 className="title">Admin Sales</h1>
             <hr className="title-line" />
+
+            <table className="content-table"> 
+                <thead>
+                    <tr>
+                        <th>Month</th>
+                        <th>Year</th>
+                        <th>Total Views</th>
+                        <th>Sales</th>
+                        <th>Revenue</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.month}</td>
+                            <td>{item.year}</td>
+                            <td>{item.totalViews}</td>
+                            <td>{item.sales}</td>
+                            <td>{item.revenue}</td>
+                        </tr>
+                    ))}
+                </tbody>
+
+            </table>
         </div>
     );
 };
